d49eaca3bee352bd4a1d16f3ed0c5198e503245b
MuneebZafarQureshi <muneebzafar2@yahoo.com>
2020-06-22 15:41:53 +0500

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Header = () => {
     return (
         <h2>
-            Expense Tracker By Muneeb
+            Expense Tracker By Muneeb Zafar
         </h2>
     )
 }
